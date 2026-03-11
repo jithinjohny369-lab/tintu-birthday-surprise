213c777ed7770eabfa7b611caa781eb55a7caf34
jithinjohny369-lab <jithinjohny369@gmail.com>
2026-03-11 22:26:44 +0530

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirthdaySurprise.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Chat()
+        {
+            return View();
+        }
+
+        public IActionResult Photos()
+        {
+            return View();
+        }
+
+        public IActionResult Letter()
+        {
+            return View();
+        }
+        public IActionResult Timeline()
+        {
+            return View();
+        }
+        public IActionResult Final()
+        {
+            return View();
+        }
+    }
+}
